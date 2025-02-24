b97615f2548a5774210caf7dd6f4b13fe70f760a
jatin-node <jatin310522@gmail.com>
2025-02-24 23:26:03 +0530

    changes made

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -52,7 +52,7 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
@@ -89,7 +89,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
